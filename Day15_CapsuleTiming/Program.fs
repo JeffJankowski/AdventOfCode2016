490f7b538c9a3ca1b3c8d717a6faae6c00e20da5
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2016-12-14 22:28:05 -0800

    day 15 completed
    easy peasy problem. Shouldn't have wrote logic to parse the small input.
    Also a recursive function (instead of inf. seq -> scan -> find) probably
    would have been better.

---
+++ Day15_CapsuleTiming/Program.fs
@@ -0,0 +1,24 @@
+﻿// http://adventofcode.com/2016/day/15
+// 12/14/2016
+
+open System
+open System.IO
+open Helpers
+
+[<EntryPoint>]
+let main argv = 
+    let discs = 
+        File.ReadAllLines ("..\\..\\input.txt")
+        |> Array.map (fun s ->
+            match s with
+            | Regex @"Disc #\d has (\d+) positions; at time=0, it is at position (\d+)." 
+                [n; pos] -> (int pos, int n) )
+
+    let target = Array.mapi (fun i (p,n) -> modulo (n-i-1) n, n) discs
+
+    Seq.initInfinite id
+    |> Seq.scan (fun acc _ -> Array.map (fun (p,n) -> ((p + 1) % n, n)) acc) discs
+    |> Seq.findIndex (fun state -> state = target)
+    |> printfn "Drop capsule when t=%i"
+
+    Console.Read ()
