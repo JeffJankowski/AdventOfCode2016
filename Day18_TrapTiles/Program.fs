52b60c7e015e2419b22ea86d6f50f60c0ced44a9
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2016-12-19 15:04:20 -0800

    day 18 completed
    400,000 rows is slow, but still less than a minute, so fuck optimizing

---
+++ Day18_TrapTiles/Program.fs
@@ -0,0 +1,41 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open System
+open System.IO
+
+
+let trap lcr = 
+    match lcr with
+    | (true, true, false)
+    | (false, true, true)
+    | (true, false, false)
+    | (false, false, true) -> true
+    | _ -> false
+
+let generate line = 
+    (false::line) @ ([false])
+    |> Seq.windowed 3
+    |> Seq.map (fun arr -> trap (arr.[0], arr.[1], arr.[2]))
+    |> Seq.toList
+
+
+[<EntryPoint>]
+let main argv = 
+    let input = 
+        ".^^..^...^..^^.^^^.^^^.^^^^^^.^.^^^^.^^.^^^^^^.^...^......^...^^^..^^^.....^^^^^^^^^....^^...^^^^..^"
+        |> Seq.map (fun c -> c = '^')
+        |> Seq.toList
+    let safe = 
+        input::
+            (Seq.unfold (fun (str, n) -> 
+                if (n >= 40) then None else 
+                    let newrow = generate str
+                    Some (newrow, (newrow,n+1)) ) (input, 1)
+            |> Seq.toList)
+        |> List.collect id
+        |> List.filter (id >> not) 
+        |> List.length
+
+    printfn "Safe tiles: %i" safe
+    Console.Read ()
