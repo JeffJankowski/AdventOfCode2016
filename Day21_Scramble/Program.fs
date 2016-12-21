8a779eda4a4e6f2b9260a92435e22e9db69cb09a
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2016-12-21 00:09:24 -0800

    cleanup

--- Day21_Scramble/Program.fs
+++ Day21_Scramble/Program.fs
@@ -1,85 +1,66 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿// http://adventofcode.com/2016/day/21
+// 12/20/2016
 
 open System
 open System.IO
 open Helpers
 
 
-let swap_p (x:int) (y:int) (charr:char[]) = 
-    let tmp = charr.[x]
-    charr.[x] <- charr.[y]
-    charr.[y] <- tmp
-    charr
+let swap_p (x:int) (y:int) (chs:char[]) = 
+    let tmp = chs.[x]
+    chs.[x] <- chs.[y]
+    chs.[y] <- tmp
+    chs
 
-let swap_l (a:char) (b:char) (charr:char[]) = 
-    let x = Array.IndexOf (charr, a)
-    let y = Array.IndexOf (charr, b)
-    swap_p x y charr
+let swap_l (a:char) (b:char) (chs:char[]) = 
+    swap_p (Array.IndexOf (chs, a)) (Array.IndexOf (chs, b)) chs
 
-let rotateN (right:bool) (i:int) (charr:char[]) = 
-    let n = i % charr.Length
-    if n = 0 then charr else
+let rotateN (right:bool) (i:int) (chs:char[]) = 
+    let n = i % chs.Length
+    if n = 0 then chs else
         match right with
-        | false -> Array.append charr.[n..charr.Length-1] charr.[0..n-1]
-        | true -> Array.append charr.[charr.Length-n..charr.Length-1] charr.[0..charr.Length-n-1]
+        | false -> Array.append chs.[n..chs.Length-1] chs.[0..n-1]
+        | true -> Array.append chs.[chs.Length-n..chs.Length-1] chs.[0..chs.Length-n-1]
 
-let rotate (a:char) (charr:char[]) =
-    let i = Array.IndexOf (charr, a)
-    let n = if i >= 4 then i + 2 else i + 1
-    rotateN true n charr
+let rotate (a:char) (chs:char[]) =
+    let i = Array.IndexOf (chs, a)
+    rotateN true (if i >= 4 then i + 2 else i + 1) chs
 
-let unrotate (a:char) (charr:char[]) = 
-    [0..charr.Length-1]
-    |> List.map (fun i -> rotateN false i charr)
-    |> List.find (fun ncharr -> charr = (rotate a ncharr))
+let unrotate (a:char) (chs:char[]) = 
+    [0..chs.Length-1]
+    |> List.map (fun i -> rotateN false i chs)
+    |> List.find (fun ncharr -> chs = (rotate a ncharr))
 
-let reverse (x:int) (y:int) (charr:char[]) =
-    Array.concat [(if x = 0 then [||] else charr.[0..x-1]); 
-        Array.rev charr.[x..y]; 
-        (if y = (charr.Length-1) then [||] else charr.[y+1..charr.Length-1])]
+let reverse (x:int) (y:int) (chs:char[]) =
+    Array.concat [(if x = 0 then [||] else chs.[0..x-1]); 
+        Array.rev chs.[x..y]; 
+        (if y = (chs.Length-1) then [||] else chs.[y+1..chs.Length-1])]
 
-let move (x:int) (y:int) (charr:char[]) =
-    if x <= y then
-        Array.concat [charr.[0..x-1]; charr.[x+1..y]; charr.[x..x]; charr.[y+1..charr.Length-1]]
-    else
-        Array.concat [charr.[0..y-1]; charr.[x..x]; charr.[y..x-1]; charr.[x+1..charr.Length-1]]
+let move (x:int) (y:int) (chs:char[]) =
+    if x <= y then Array.concat [chs.[0..x-1]; chs.[x+1..y]; chs.[x..x]; chs.[y+1..chs.Length-1]]
+    else Array.concat [chs.[0..y-1]; chs.[x..x]; chs.[y..x-1]; chs.[x+1..chs.Length-1]]
 
-let scramble (seed:string) instructions = 
+let scramble (un:bool) (seed:string) instructions = 
     instructions
+    |> if un then Array.rev else Array.map id
     |> Array.fold (fun scr ins -> 
             match ins with
             | Regex @"swap position (\d) with position (\d)" [x;y] -> swap_p (int x) (int y) scr
             | Regex @"swap letter ([a-z]) with letter ([a-z])" [a;b] -> swap_l (char a) (char b) scr
-            | Regex @"rotate (left|right) (\d) steps?" [dir;n] -> rotateN (dir = "right") (int n) scr
-            | Regex @"rotate based on position of letter ([a-z])" [a] -> rotate (char a) scr
+            | Regex @"rotate (left|right) (\d) steps?" [dir;n] -> 
+                rotateN (if un then dir = "left" else dir = "right") (int n) scr
+            | Regex @"rotate based on position of letter ([a-z])" [a] -> 
+                if un then unrotate (char a) scr else rotate (char a) scr
             | Regex @"reverse positions (\d) through (\d)" [x;y] -> reverse (int x) (int y) scr
-            | Regex @"move position (\d) to position (\d)" [x;y] -> move (int x) (int y) scr
-            ) (seed.ToCharArray())
-    |> String.Concat
-
-let unscramble (seed:string) instructions = 
-    instructions
-    |> Array.rev
-    |> Array.fold (fun scr ins -> 
-            match ins with
-            | Regex @"swap position (\d) with position (\d)" [x;y] -> swap_p (int x) (int y) scr
-            | Regex @"swap letter ([a-z]) with letter ([a-z])" [a;b] -> swap_l (char a) (char b) scr
-            | Regex @"rotate (left|right) (\d) steps?" [dir;n] -> rotateN (dir = "left") (int n) scr
-            | Regex @"rotate based on position of letter ([a-z])" [a] -> unrotate (char a) scr
-            | Regex @"reverse positions (\d) through (\d)" [x;y] -> reverse (int x) (int y) scr
-            | Regex @"move position (\d) to position (\d)" [x;y] -> move (int y) (int x) scr
+            | Regex @"move position (\d) to position (\d)" [x;y] -> 
+                if un then move (int y) (int x) scr else move (int x) (int y) scr
             ) (seed.ToCharArray())
     |> String.Concat
 
 [<EntryPoint>]
 let main argv = 
     let input = File.ReadAllLines ("..\\..\\input.txt")
-
-    let scrambled = scramble "abcdefgh" input
-    printfn "'abcdefgh' scrambled:    %s" scrambled
-
-    let unscrambled = unscramble "fbgdceah" input
-    printfn "'fbgdceah' unscrambled:  %s" unscrambled
+    printfn "'abcdefgh' scrambled:    %s" (scramble false "abcdefgh" input)
+    printfn "'fbgdceah' unscrambled:  %s" (scramble true "fbgdceah" input)
 
     Console.Read ()
