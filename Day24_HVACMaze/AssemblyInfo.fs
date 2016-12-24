7b393419fdb59e7c87cc76ab5105b0704654b5bb
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2016-12-24 01:45:26 -0800

    day 24 completed
    nasty ol' imperative BFS, then brute-force TFS. i'm not dealing with
    graph traversals functionally...

---
+++ Day24_HVACMaze/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Day24_HVACMaze.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Day24_HVACMaze")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("EP")>]
+[<assembly: AssemblyProduct("Day24_HVACMaze")>]
+[<assembly: AssemblyCopyright("Copyright © EP 2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("2ca2ddfb-23bd-4e59-9dac-34200c79cf76")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Day24_HVACMaze/Program.fs
@@ -0,0 +1,57 @@
+﻿// http://adventofcode.com/2016/day/24
+// 12/23/2016
+
+open System
+open System.IO
+open System.Collections.Generic
+open Helpers
+
+
+let solve (start: int*int) (finish: int*int) (map: bool[][]) = 
+    let q = new Queue<(int*int)*int>([(start,0)]);
+    let set = new HashSet<int*int> ()
+    let mutable min = None
+    while min.IsNone do
+        let ((x,y),n) = q.Dequeue ()
+        if (x,y) = finish then min <- Some n else
+            [(x+1,y); (x-1,y); (x,y-1); (x,y+1)]
+            |> List.filter (fun (x0,y0) -> 
+                x0 >= 0 && y0 >= 0 && y0 < map.Length && x0 < map.[0].Length && 
+                map.[y0].[x0] && (set.Contains (x0,y0) |> not))
+            |> List.iter (fun p -> 
+                set.Add (p) |> ignore
+                q.Enqueue (p,(n+1)))
+    min.Value
+
+[<EntryPoint>]
+let main argv = 
+    let targets = Array.zeroCreate<int*int> 8
+    let map = 
+        File.ReadAllLines ("..\\..\\input.txt")
+        |> Array.mapi (fun y s ->
+            s.ToCharArray()
+            |> Array.mapi (fun x c ->
+                match c with
+                | '#' -> false
+                | '.' -> true
+                | p -> targets.[(int (p.ToString()))] <- (x,y); true))
+
+    let dists =
+        comb 2 [0..7]
+        |> List.map (fun l -> (l.[0], l.[1]))
+        |> List.map (fun (s,e) -> (s,e), (solve targets.[s] targets.[e] map ))
+        |> dict
+
+    let min perms = 
+        perms
+        |> List.map (fun l ->
+            List.pairwise l
+            |> List.map (fun (i,j) -> if i < j then dists.[i,j] else dists.[j,i])
+            |> List.sum )
+        |> List.min
+        
+    let paths = permute [1..7] |> List.map (fun l -> 0::l)
+    printfn "Min path steps:  %i" (min paths)
+    printfn "Min cycle steps: %i" (min (List.map (fun l -> l @ [0]) paths))
+
+    Console.Read ()
