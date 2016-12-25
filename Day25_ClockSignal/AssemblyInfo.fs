75cc955a430eee723e782d65bc5036085b9dce97
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2016-12-25 15:40:21 -0800

    day 25 completed
    more assembunny, way easy

---
+++ Day25_ClockSignal/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Day25_ClockSignal.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Day25_ClockSignal")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("EP")>]
+[<assembly: AssemblyProduct("Day25_ClockSignal")>]
+[<assembly: AssemblyCopyright("Copyright © EP 2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("e14a7f0e-f304-48d6-b771-501e378415a1")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Day25_ClockSignal/Program.fs
@@ -0,0 +1,64 @@
+﻿// http://adventofcode.com/2016/day/25
+// 12/25/2016
+
+open System
+open System.IO
+open Helpers
+
+let a = ref 0
+let b = ref 0
+let c = ref 0
+let d = ref 0
+let ic = ref 0
+
+let reg s =
+    match s with
+    | "a" -> a
+    | "b" -> b
+    | "c" -> c
+    | "d" -> d
+
+let regval s =
+    let x = ref 0
+    if Int32.TryParse (s, x) then x.Value else (reg s).Value
+
+
+let run (instr: string[]) = 
+    seq { 
+        ic := 0
+        while ic.Value < instr.Length do
+            match instr.[ic.Value] with
+            | Regex @"cpy (.+) ([a-z])" [v; r] -> if Char.IsLetter(r.Chars 0) then (reg r) := (regval v)
+            | Regex @"inc ([a-z])" [r] -> 
+                if Char.IsLetter(r.Chars 0) then 
+                    let i = ic.Value
+                    match (sprintf "%s %s %s %s" instr.[i+1] instr.[i+2] instr.[i+3] instr.[i+4]) with
+                    | Regex @"dec ([a-z]) jnz ([a-z]) -2 dec ([a-z]) jnz ([a-z]) -5" [a; a1; b; b1] 
+                        when a = a1 && b = b1 -> 
+                            ic := ic.Value + 4
+                            (reg r) := (reg r).Value + ((int (reg a).Value) * (int (reg b).Value))
+                    | _ -> (reg r) := (reg r).Value + 1
+            | Regex @"dec ([a-z])" [r] -> if Char.IsLetter(r.Chars 0) then (reg r) := (reg r).Value - 1
+            | Regex @"jnz (.+) (.+)" [v; w] -> if (regval v) <> 0 then ic := ic.Value + (regval w) - 1
+            | Regex @"out (.+)" [v] -> yield (regval v)
+            ic := ic.Value + 1 }
+
+[<EntryPoint>]
+let main argv = 
+    let input = File.ReadAllLines ("..\..\input.txt")
+    let min = 
+        Seq.initInfinite id
+        |> Seq.pick (fun i ->
+            a := i
+            b := 0
+            c := 0
+            d := 0
+            let good = 
+                run input
+                |> Seq.take 10
+                |> Seq.chunkBySize 2
+                |> Seq.forall (fun chk -> chk.[0] = 0 && chk.[1] = 1)
+            if good then Some i else None )
+    printfn "Repeating signal when reg 'a' = %i" min
+
+    Console.Read ()
